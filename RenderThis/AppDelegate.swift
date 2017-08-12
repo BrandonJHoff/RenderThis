621921e59db9b07c7fbccfd1f591603c32c026ea
Brandon Hoff <bhoff@purch.com>
2017-08-11 20:23:51 -0600

    Initial Commit

---
+++ RenderThis/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  RenderThis
+//
+//  Created by Brandon Hoff on 8/11/17.
+//  Copyright © 2017 Brandon Hoff. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ RenderThis/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  RenderThis
+//
+//  Created by Brandon Hoff on 8/11/17.
+//  Copyright © 2017 Brandon Hoff. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
